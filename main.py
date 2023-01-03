6cbe8eecabe5a7a04236b9b0d04c4494feda4d2f
forczc <forczc@foxmail.com>
2023-01-03 17:42:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         message_all = f"{message_all}{message}\n"
 
     if -2 not in checkin_codes and checkin_codes.count(0) + checkin_codes.count(1) == len(checkin_codes):
-        title = "GLaDOS(8726..) success"
+        title = "GLaDOS(8726..) finished"
     else:
         title = "GLaDOS(8726..) fail"
     message_all = f"{title}\n{message_all}"
